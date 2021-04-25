78fa46ea0841ed00d76f63e6555fb715515abe8f
kubistika <kmizrachi18@gmail.com>
2021-04-25 15:04:27 +0300

    update code

--- first-fn-with-pipelines/index.ts
+++ first-fn-with-pipelines/index.ts
@@ -3,7 +3,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: 'Hi, ya ah!'
+        body: 'Hi i love tamar'
     };
 
 };
